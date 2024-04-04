c0c5d08057a86048fc4d5177676379ce4a954174
zweirdo <1606403438@qq.com>
2024-04-04 23:52:11 +0800

    refactor: 重构Dropdown组件

--- src/components/Dropdown/Dropdown.vue
+++ src/components/Dropdown/Dropdown.vue
@@ -1,37 +1,83 @@
 <template>
-  <div class="el-dropdown" @click="toggleDropdown">
-    <div class="el-dropdown-trigger">
-      <slot name="trigger"></slot>
-    </div>
+  <!-- DropDown的模板结构以Tooltop作为基础，菜单选项作为插槽传入，遍历菜单项，
+    如果传入的菜单项为divided情况下，生成分隔线，然后渲染item.label，即菜单项。 -->
 
-    <transition name="el-fade-in">
-      <div v-if="visible" class="el-dropdown-menu">
-        <slot name="content"></slot>
-      </div>
-    </transition>
+  <div class="el-dropdown">
+    <Tooltip
+      :trigger="trigger"
+      :placement="placement"
+      :popper-options="popperOptions"
+      :open-delay="openDelay"
+      :close-delay="closeDelay"
+      :manual="manual"
+      @visible-change="visibleChange"
+      ref="tooltipRef"
+    >
+      <slot />
+      <template #content>
+        <ul class="el-dropdown__menu">
+          <!-- 功能：滑入后有菜单项显示，基于Tooltip组件进行二次开发
+                Props: 传入menuOptions数组作为props, 每一个菜单项包含以下内容
+                label: 显示内容、字符串或者VNode 
+                key: 键值
+                disabled: 是否禁用
+                divided: 是否有分隔符 
+              -->
+          <template v-for="item in menuOptions" :key="item.key">
+            <!-- divided情况下 添加分隔线 -->
+            <li v-if="item.divided" role="separator" class="divided-placeholder" />
+            <!-- 点击 菜单项，emit select事件 -->
+            <li
+              class="el-dropdown__item"
+              @click="itemClick(item)"
+              :class="{ 'is-disabled': item.disabled, 'is-divided': item.divided }"
+              :id="`dropdown-item-${item.key}`"
+            >
+              <!-- 渲染 label 字符串 或者 VNode -->
+              <!-- {{  item.label }} -->
+              <RenderVnode :vNode="item.label" />
+            </li>
+          </template>
+        </ul>
+      </template>
+    </Tooltip>
   </div>
 </template>
-
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
-const visible = ref(false)
-const toggleDropdown = () => {
-  visible.value = !visible.value
-}
-// 关闭下拉框的事件监听
-const closeDropdown = () => {
-  visible.value = false
-}
-// 添加全局点击事件监听器
-onMounted(() => {
-  document.addEventListener('click', closeDropdown)
-})
-onUnmounted(() => {
-  document.removeEventListener('click', closeDropdown)
-})
+import { ref } from 'vue'
+import type { DropdownProps, DropdownInstance, DropdownEmits, MenuOption } from './types'
+import Tooltip from '../Tooltip/Tooltip.vue'
+/**
+ * 但是直接使用插值表达式只能渲染字符串形式的label, 不支持传入更复杂的虚拟DOM,
+ * 这里我们将之定义成一个组件RenderVnode
+ */
+import RenderVnode from '../Common/RenderVnode'
+import type { TooltipInstance } from '../Tooltip/types'
 defineOptions({
   name: 'ElDropdown'
 })
+const props = withDefaults(defineProps<DropdownProps>(), { hideAfterClick: true })
+const emits = defineEmits<DropdownEmits>()
+const tooltipRef = ref<TooltipInstance | null>(null)
+const visibleChange = (e: boolean) => {
+  // 控制菜单项是否显示
+  emits('visible-change', e)
+}
+// 点击 菜单项
+const itemClick = (e: MenuOption) => {
+  // 处理禁用状态下的菜单项
+  if (e.disabled) {
+    return
+  }
+  // emit select事件 选择菜单项
+  emits('select', e)
+  // 点击之后是否隐藏
+  if (props.hideAfterClick) {
+    tooltipRef.value?.hide()
+  }
+}
+defineExpose<DropdownInstance>({
+  show: () => tooltipRef.value?.show(),
+  hide: () => tooltipRef.value?.hide()
+})
 </script>
-
-<style scoped></style>
--- src/components/Dropdown/types.ts
+++ src/components/Dropdown/types.ts
@@ -1,20 +1,25 @@
 import type { VNode } from 'vue'
 import type { TooltipProps } from '../Tooltip/types'
 
-export interface DropdownProps extends TooltipProps {
-  menuOptions: MenuOption[]
-}
+// MenuOption内容，包含 标签、键、是否禁用、是否有分隔符号
 export interface MenuOption {
   label: string | VNode
   key: string | number
   disabled?: boolean
-  divided: boolean
+  divided?: boolean
+}
+
+// 传入菜单项数组作为 Props
+export interface DropdownProps extends TooltipProps {
+  menuOptions: MenuOption[]
+  hideAfterClick?: boolean
 }
 
 export interface DropdownEmits {
   (e: 'visible-change', value: boolean): void
   (e: 'select', value: MenuOption): void
 }
+
 export interface DropdownInstance {
   show: () => void
   hide: () => void
