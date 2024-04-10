3a9442e90205bcbe60c831ae06d315753a8492a1
zweirdo <1606403438@qq.com>
2024-04-10 17:38:52 +0800

    deploy

---
+++ assets/chunks/RenderVnode.wYSMewFV.js
@@ -0,0 +1,1 @@
+import{d as r}from"./framework.D1lKWgLe.js";const t=r({props:{vNode:{type:[String,Object],required:!0}},setup(e){return()=>e.vNode}});export{t as R};
---
+++ assets/components_basic.md.Dx3Z9fp6.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.D1lKWgLe.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"components/basic.md","filePath":"components/basic.md"}'),o={name:"components/basic.md"};function s(c,n,r,i,p,m){return a(),t("div")}const f=e(o,[["render",s]]);export{d as __pageData,f as default};
