6ee371ab36dd61ba036b3f26c949f82b65fa2c62
zweirdo <1606403438@qq.com>
2024-04-10 16:22:16 +0800

    feat: 部署github pages

---
+++ vitepress-starter/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~
+git push -f git@github.com/2WeirDo/weirdo-ui.git master:gh-pages
+
+cd -
